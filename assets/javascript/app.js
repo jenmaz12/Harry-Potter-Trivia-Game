e804e61dd3edb1b29df1941e323ba75a2298b61c
jenmaz12 <jenmaz12@gmail.com>
2018-11-25 09:42:30 -0500

    trying to run timer and show question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,38 @@ $(window).ready(function(){
 
 $("button").on("click",function(){
     var i;
-    for(i=0; i < 11; i++) {
+    for(i=1; i < 11; i++) {
+        // show trivia question and timer
         $("#trivia"+i).show();
+        $("#time").show();
         
-    }
-})
+        // set counter to 30
+        var number = 30;
+        var intervalId;
+        
+        // define run timer function
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+          };
+        
+        // define decrement function
+          function decrement() {
+            //  Decrease number by one.
+            number--;
+
+            //  Show the number in the #time tag.
+            $("#time").html("<h2>" + number + "</h2>");
+
+            //  Once number hits zero...
+            if (number === 0) {
+
+                //  ...run the stop function.
+                stop();
+
+                //  Alert the user that time is up.
+                alert("Time Up!");
+            };
+        };
+        run ();
+}});
